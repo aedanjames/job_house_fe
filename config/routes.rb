04be54077a007660109b61f423cc9718fc1e4bf7
Eldridge Turambi <eturambi95@gmail.com>
2022-04-15 11:36:04 -0600

    Update routes: Still not working

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/auth/google_oauth2/callback', to: 'users#authorize'
+  get '/search/jobs', to: 'searches#jobs_by_location'
 end
