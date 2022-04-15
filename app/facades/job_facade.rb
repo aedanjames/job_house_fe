2e7bd4f22bf22a0446dd30b5f65c7fd60b16c787
Eldridge Turambi <eturambi95@gmail.com>
2022-04-15 11:35:45 -0600

    Update Job: Service, Facade, Poro framework

---
+++ app/facades/job_facade.rb
@@ -0,0 +1,12 @@
+class JobFacade
+  class << self
+
+    def jobs_by_location(location)
+      jobs_json = JobService.jobs_by_location(location)
+
+      jobs_json[:data].map do |job_data|
+        Job.new(job_data)
+      end
+    end
+  end
+end
---
+++ app/services/job_service.rb
@@ -0,0 +1,19 @@
+class JobService
+  class << self
+
+    def connection
+      Faraday.new('http://localhost:5000/api/v1/')
+    end
+
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def jobs_by_location(location)
+      response = connection.get('jobs') do |faraday|
+        faraday.params[:where] = location
+      end
+      parse_json(response)
+    end
+  end
+end
