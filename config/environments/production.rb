0e43517c20da38251700043c32dc7554556443a7
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-27 14:59:03 -0600

    fix: commented out config line naming wrong app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "viewing_party_lite_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "job_house_fe#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
