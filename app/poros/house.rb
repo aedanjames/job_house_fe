3424554fce75a052abc87df4c7dc1fe226f83deb
Aedan Yturralde <aedanyturralde@gmail.com>
2022-05-06 10:17:48 -0600

    add city and state to house poro address

--- app/poros/house.rb
+++ app/poros/house.rb
@@ -1,6 +1,7 @@
 class House
   attr_reader :listing_price,
               :address,
+              :city_state,
               :contact,
               :photos,
               :bedrooms, 
@@ -14,7 +15,8 @@ class House
   def initialize(data)
     @mls_id = data[:id]
     @listing_price = data[:attributes][:list_price]
-    @address = data[:attributes][:address]
+    @address = "#{data[:attributes][:address]} #{data[:attributes][:city]}, #{data[:attributes][:state]}"
+    @city_state = "#{data[:attributes][:city]}, #{data[:attributes][:state]}"
     @contact = data[:attributes][:contact]
     @bedrooms = data[:attributes][:bedrooms]
     @half_baths = data[:attributes][:half_baths]
